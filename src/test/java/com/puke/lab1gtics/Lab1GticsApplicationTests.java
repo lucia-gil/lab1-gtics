daae9be2950848dcc69045f110d5a6de6bf7a686
lucia-gil <a20233424@pucp.edu.pe>
2026-08-20 19:56:30 -0500

    mi primer commit en esta repo

---
+++ src/test/java/com/puke/lab1gtics/Lab1GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puke.lab1gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1GticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
