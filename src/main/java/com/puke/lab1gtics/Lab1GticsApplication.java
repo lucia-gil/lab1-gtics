04b50e7649c661f68559fd688729525a884ead89
lucia-gil <a20233424@pucp.edu.pe>
2026-08-20 20:07:20 -0500

    segundo commit

--- src/main/java/com/puke/lab1gtics/Lab1GticsApplication.java
+++ src/main/java/com/puke/lab1gtics/Lab1GticsApplication.java
@@ -9,5 +9,5 @@ public class Lab1GticsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1GticsApplication.class, args);
 	}
-
+		System.out.println("Hola mundito")
 }
